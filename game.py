7ccaba88cd8789fc5bf1851c89864f70fad6b84e
thomas rodrigues <thomas.quamtum@gmail.com>
2021-09-11 20:39:33 -0500

    i created new branch to fix the structure of one before / i have a bug whit the function enumerate

--- game.py
+++ game.py
@@ -1,45 +1,26 @@
 import os
 import time
-
-def print_structure(leters_said = [], word= "cafe"):
-        os.system("cls")
-        print("!adivina la palabra¡")
-        print("\n")
-
-        for leter in word:
-            back_slash = (lambda leter,leters_said : leter if leter in leters_said else "_") #put or not the back slash
-            print(back_slash(leter,leters_said),end="")
-
-        print("\n")
-
-def end(word):
-    print(f"gracias por jugar la palabra era {word}")
-
-def game(leters_said,word):
-    word_list = [i for i in word]
-    print_structure(leters_said,word)
-    end(word) if [i for i in word_list] else None
-    leter_imput = input("pon la letra: ") #problem of loop
-    if leter_imput not in leters_said:
-        leters_said.append(leter_imput)
-
-    elif leter_imput in leters_said:
-        print("ya has puesto esa letra")
-        time.sleep(5)
-
-    else:
-        print(f"unknow error/ info[ imput:{leter_imput} leter said{leters_said} ]")   
-
-    game(leters_said,word)   
-
+import random
+from functools import reduce
 
 def run():
-    word_ = "cafe"
-    leters_said_ = []
-    game(leters_said_,word_)
+    def select_word():
+        with open("Words.txt","r",encoding="utf-8") as file:
+            words = [line for line in file.readlines()]
+            words = list(map(lambda word : word.strip(),words)) # remove the \n
+            words = list(enumerate(words))
+            print(words)
+        with open("Words.txt","r",encoding="utf-8") as file:
+            N_lines =  reduce(lambda a, b: a + b,[1 for line in file])
+            line_choser  = random.randint(1, N_lines+1)
+            return [item[1] for item in words if item[0] == line_choser]
+            
 
-    
-    
+    print(select_word())
+
+
+
+
 
 if __name__ == '__main__':
     run()
