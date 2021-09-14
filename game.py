a9007b5f6376c23514c9fc1b0d5d91e6b0e4c325
thomas rodrigues <thomas.quamtum@gmail.com>
2021-09-14 18:11:54 -0500

    i created a map for eliminating the strange charaxcters

--- game.py
+++ game.py
@@ -5,15 +5,18 @@ from functools import reduce
 
 def run():
     def select_word():
+        adjusted_word = {"Ã³":"o", "Ã±": "ñ", "Ã©" : "e", "Ã":"i" }
         with open("Words.txt","r",encoding="utf-8") as file:
             words = [line for line in file.readlines()]
             words = list(map(lambda word : word.strip(),words)) # remove the \n
-            words = list(enumerate(words))
-            print(words)
+            words = list(enumerate(words)) #enumerate the words s
         with open("Words.txt","r",encoding="utf-8") as file:
             N_lines =  reduce(lambda a, b: a + b,[1 for line in file])
             line_choser  = random.randint(1, N_lines+1)
-            return [item[1] for item in words if item[0] == line_choser]
+        word = ([item[1] for item in words if item[0] == line_choser]) #return the word wer
+        word = list(map(lambda leter: adjusted_word[leter] if leter in adjusted_word.keys() else leter,word)) #change the wrong leters
+
+        return "".join(word)
             
 
     print(select_word())
