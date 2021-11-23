60fd53b53cbfe26f965dceb5977604bd1e6ffa78
thomas rodrigues <thomas.quamtum@gmail.com>
2021-11-23 08:42:50 -0600

    raw structure of funcionalitis

--- game.py
+++ game.py
@@ -8,11 +8,12 @@ def select_word():
     to_replace = {"Ã³":"o", "Ã±": "ñ", "Ã©" : "e", "Ã":"i" }
     with open("Words.txt","r",encoding="utf-8") as file:
         words = [line for line in file.readlines()]
-        words = list(map(lambda word : word.strip(),words)) # remove the \n
-        words = list(enumerate(words)) #enumerate the words s
 
-    line_choser  = random.randint(1, words[-1][0]+1) 
-    word = "".join([item[1] for item in words if item[0] == line_choser]) #return the word wer
+    words = list(map(lambda word : word.strip(),words)) # remove the \n
+    words = list(enumerate(words))
+
+    line_choser  = random.randint(1, len(words)) 
+    word = "".join([item[1] for item in words if item[0] == line_choser]) #return the word selected
     for a,b in to_replace.items():
         word = word.replace(a,b)
     return "".join(word)
@@ -25,15 +26,17 @@ def next_game(word):
 
 def print_estucture(word = select_word()):
     leter_said = []
+    word = list(word)
     while True:
         os.system("clear")
         if sum([letra in leter_said for letra in word]) == len(word): next_game(word)
         print("!adivina la palabra¡")
+        print(word)
         print("\n")
         back_slash = "".join(["_" if leter not in leter_said else leter for leter in word])
         print(back_slash)
         user_leter = input("put a leter: ")
-        leter_said.append(user_leter)
+        if user_leter in word : leter_said.append(user_leter)
 
 def run():
     print_estucture()
